25f8ac5a9c7ac4cb5be8c714eafb2d67432a3ce3
black-devil-369 <ajaychauhan1262@gmail.com>
2024-05-01 15:16:50 +0530

    contact page

--- my_portfolio/app/contact-us/page.jsx
+++ my_portfolio/app/contact-us/page.jsx
@@ -1,5 +1,35 @@
+import { Button, Checkbox, Label, TextInput } from "flowbite-react";
 import React from "react";
-
+//import "./contact.css";
 export default function Contact() {
-  return <div>Contact-Us page</div>;
+  return (
+    <div id="form">
+      <form className=" bg-slate-100 shadow-lg shadow-stone-300 px-60 py-56 rounded-lg">
+        <div className="flex justify-center  flex-col gap-4">
+          <div>
+            <div>
+              <Label htmlFor="email" value="Your Email" />
+            </div>
+            <TextInput
+              id="email"
+              type="email"
+              placeholder="yourname@0000gmail.com"
+              required
+            />
+          </div>
+          <div>
+            <div>
+              <Label htmlFor="password" value="Your Password" />
+            </div>
+            <TextInput id="password" type="password" required />
+          </div>
+          <div>
+            <Checkbox id="remember" />
+            <Label htmlFor="remember">Remember me</Label>
+          </div>
+          <Button type="sumbit">Submit</Button>
+        </div>
+      </form>
+    </div>
+  );
 }
